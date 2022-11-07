8270a18c8d9046d118261c8fc1665311209167f7
panyinglang <1365972041@qq.com>
2022-11-07 17:22:35 +0800

    update premake script

--- premake5.lua
+++ premake5.lua
@@ -9,13 +9,24 @@ project "GLFW"
 	{
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
+		"src/internal.h",
+		"src/platform.h",
+    	"src/mappings.h",
+
+    	"src/init.c",
+    	"src/input.c",
+    	"src/monitor.c",
 		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
+		"src/platform.c",
 		"src/vulkan.c",
-		"src/window.c"
+		"src/window.c",
+
+		"src/null_platform.h",
+		"src/null_joystick.h",
+        "src/null_init.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c",
 	}
 
 	filter "system:windows"
@@ -24,11 +35,18 @@ project "GLFW"
 
 		files
 		{
+			"src/win32_platform.h",
+			"src/win32_joystick.h",
+			"src/wgl_context.h",
+			"src/egl_context.h",
+			"src/osmesa_context.h",
+
 			"src/win32_init.c",
 			"src/win32_joystick.c",
 			"src/win32_monitor.c",
 			"src/win32_time.c",
 			"src/win32_thread.c",
+			"src/win32_module.c",
 			"src/win32_window.c",
 			"src/wgl_context.c",
 			"src/egl_context.c",
@@ -40,11 +58,6 @@ project "GLFW"
 			"_GLFW_WIN32",
 			"_CRT_SECURE_NO_WARNINGS"
 		}
-
-        postbuildcommands --编译后自定义命令
-        {
-            ("{COPY} %{cfg.buildtarget.relpath} %{wks.location}/bin/" .. outputdir .. "/FakeRealEditor") --lib库到FakeRealEditor.exe的同一目录下去
-        }
 
 	filter "configurations:Debug"
 		runtime "Debug"
